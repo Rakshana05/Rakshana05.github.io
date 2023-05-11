1be3075c65102afa653cab056a4040ede3f7552b
Rakshana05 <114346437+Rakshana05@users.noreply.github.com>
2023-05-11 14:26:50 +0530

    Update game.js

--- game.js
+++ game.js
@@ -239,7 +239,7 @@ const gameBoard = document.getElementById("board")
 const main = currentTime => {
     if(gameOver){
         if(confirm('You lost. Press ok to restart')){
-            window.location = '/'
+            location.reload()
         }
         
         return
